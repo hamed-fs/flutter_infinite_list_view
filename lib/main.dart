0a54e013d04b50c42e0ebac58597dde086252716
Hamed <hamed@firstsource.io>
2019-12-22 11:19:05 +0330

    - add infinit list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:progress_indicators/progress_indicators.dart';
 
 void main() => runApp(App());
 
@@ -9,7 +10,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: HomePage(title: 'Flutter Demo Home Page'),
     );
@@ -26,13 +27,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final int _pageSize = 10;
+
+  int _currentMax = 10;
+  List<String> _list;
+
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    _list = List.generate(_pageSize, (int index) => 'Item: ${index + 1}');
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        getMoreData();
+      }
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: _list.length + 1,
+        itemExtent: 80.0,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == _list.length) {
+            return JumpingDotsProgressIndicator(
+              color: Theme.of(context).accentColor,
+              fontSize: 48.0,
+            );
+          }
+
+          return ListTile(
+            title: Text(_list[index]),
+          );
+        },
+      ),
     );
+  }
+
+  Future<void> getMoreData() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+
+    for (int i = _currentMax; i < _currentMax + _pageSize; i++) {
+      _list.add('Item: ${i + 1}');
+    }
+
+    _currentMax += _pageSize;
+
+    setState(() {});
   }
 }
